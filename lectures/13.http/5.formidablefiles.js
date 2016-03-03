8e45381dbd6351dff225cef8091a6707e731e2a2
onlove <505566896@qq.com>
2016-03-03 10:00:27 +0800

    dfdss

--- lectures/13.http/5.formidablefiles.js
+++ lectures/13.http/5.formidablefiles.js
@@ -4,34 +4,40 @@
 /**
  * Created by Administrator on 2016/3/2.
  */
+/**
+ * Created by Administrator on 2016/3/2.
+ */
 var http=require('http')
 var url=require('url');
 var querystring=require('querystring');
-var formidable=require('formidable')
+var formidable=require('formidable');
+var mime=require('mime');
 var fs=require('fs');
 var user=[];
 var server=http.createServer(function(req,res){
     var urlObj=url.parse(req.url,true);
     var pathname=urlObj.pathname;
     if(pathname=="/"){
-        fs.createReadStream('./index.html').pipe(res)
+        fs.createReadStream('./h5.html').pipe(res)
     }else if(pathname=='/reg'){
         var form=new formidable.IncomingForm();
         form.parse(req,function(err,fields,files){
-           // console.log(fields)
-            //console.log(files);
-            res.setHeader('Content-Type','text/html;charset=utf-8')
-            fs.createReadStream(files.avtor.path).pipe(fs.createWriteStream('./upload/'+files.avtor.name))
-            res.write(JSON.stringify(fields));
-            res.end('<img src="/upload/'+files.avtor.name+'">');
+            var filenames=[];
+            for(var filename in files){
+                fs.createReadStream(files[filename].path).pipe(fs.createWriteStream('./upload/'+files[filenames].name))
+                filename.push("/upload/"+files[filenames].name)
+            }
+            res.end(JSON.stringify(filenames))
         })
     }else if(pathname=='/favicon.ico'){
-        res.end();
+        return res.end('404');
     }else{
-
+        res.setHeader('Content-Type',mime.lookup(pathname))
         fs.createReadStream('.'+pathname).pipe(res);
     }
 
 
 });
 server.listen(8081);
+
+
